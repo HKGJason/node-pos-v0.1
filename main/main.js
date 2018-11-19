64bd9b412f179260c9a38757e632554170045158
J <jason19951020@gmail.com>
2018-11-20 00:26:26 +0800

    finish

--- main/main.js
+++ main/main.js
@@ -11,27 +11,30 @@ function printInventory(inputs){
 			if(inputs[i].name === allitem[j].name){
 				occured = true;
 				allitem[j].count = (parseInt(allitem[j].count)) + 1;
+				allitem[j].total = (parseInt(allitem[j].total))+ (parseInt(inputs[i].price));
 				break;
 			}
 		}
 		if (occured === false){
-			var data = {name:inputs[i].name, unit:inputs[i].unit, count:1, price:inputs[i].price};
+			var data = {name:inputs[i].name, unit:inputs[i].unit, count:1, price:(parseInt(inputs[i].price)), total:inputs[i].price};
 			allitem.push(data);
 		}
 	}
 	
 	
 	
-	var total = 0;
+	var checkout = 0;
 
 	for (var c = 0 ; c < allitem.length ; c ++){
-	    var sum = ((parseInt(allitem[c].unit))*(parseInt(allitem[c].price)));
-		str = str + ("名称："+allitem[c].name+"，数量："+allitem[c].count+allitem[c].unit+"，单价："+allitem[c].price+".00(元)，小计："+sum+"(元)\n");
-		total = total + sum;
+
+
+	    //var sum = ((parseInt(allitem[c].unit))*(parseInt(allitem[c].price)));
+		str = str + ("名称："+allitem[c].name+"，数量："+allitem[c].count+allitem[c].unit+"，单价："+allitem[c].price+".00(元)，小计："+allitem[c].total+".00(元)\n");
+		checkout = checkout + allitem[c].total;
 	}
 	
 	str = str+('----------------------\n');
-	str = str + ('总计：'+total+'.00(元)\n');
+	str = str + ('总计：'+checkout+'.00(元)\n');
 	str = str + ('**********************');
 	
 	
